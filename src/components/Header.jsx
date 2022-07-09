84654744de8293246ea8c56a17f47f37f4f6dedc
crystian97 <icrystianveloso@gmail.com>
2022-07-09 14:17:47 -0300

    Global style config

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Logo from "../assets/Logo";
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <Logo />
+      </div>
+      <div>
+        <form action="">
+          <input type="text" name="" id="" />
+          <button>Criar</button>
+        </form>
+      </div>
+    </header>
+  );
+}
