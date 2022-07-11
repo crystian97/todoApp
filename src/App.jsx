947109e8ba55012348960e6abda87a259e0568a0
crystian97 <icrystianveloso@gmail.com>
2022-07-11 14:11:58 -0300

    Home without tasks visual designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import Logo from "./assets/Logo";
 import style from "./App.module.css";
 import Header from "./components/Header";
+import Home from "./Pages/Home";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  console.log(tasks);
   return (
     <div className={style.wrapper}>
       <Header />
+      <Home tasks={tasks} />
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-import Header from "../components/Header";
-
-export default function Home() {
+import Empty from "../assets/Empty";
+import style from "./Home.module.css";
+export default function Home({ tasks }) {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main className={style.home}>
+      <header className={style.dashTasks}>
+        <div id={style.tasksCreated}>
+          <span>Tarefas criadas</span>
+          <span className={style.counter}>0</span>
+        </div>
+        <div id={style.tasksCompleted}>
+          <span>Concluidas </span>
+          <span className={style.counter}>0</span>
+        </div>
+      </header>
+      <div className={style.content}>
+        {tasks.length > 0 ? (
+          <div>tasks</div>
+        ) : (
+          <div className={style.emptyNotification}>
+            <Empty />
+            <span>
+              <strong> Você ainda não tem tarefas cadastradas</strong> <br /> Crie tarefas e
+              organize seus itens a fazer
+            </span>
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
